fc0ef8b176b265a930147bf02b8047a433d7046a
liqiit <liqiit@163.com>
2021-01-04 11:48:00 +0800

    增加对mongodb数据变化监听

---
+++ mongodb-study/springboot-demo/src/main/java/com/mongodb/ApplicationStart.java
@@ -0,0 +1,19 @@
+package com.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Title: ApplicationStart
+ * Description:
+ * Company: iFree Group
+ *
+ * @author liqi
+ * @date 2021/1/4
+ */
+@SpringBootApplication
+public class ApplicationStart {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationStart.class, args);
+    }
+}
